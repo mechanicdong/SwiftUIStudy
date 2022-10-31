d230b716f08ec1a90d3149ed304ca740e40c0e7c
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 17:46:50 +0900

    [ADD] View Blur, InnerShadow Extension

---
+++ AnimatedWeather/AnimatedWeather/Models/Components/Blur.swift
@@ -0,0 +1,20 @@
+//
+//  Blur.swift
+//  AnimatedWeather
+//
+//  Created by 이동희 on 2022/10/31.
+//
+
+import SwiftUI
+
+struct Blur: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Blur_Previews: PreviewProvider {
+    static var previews: some View {
+        Blur()
+    }
+}
---
+++ AnimatedWeather/AnimatedWeather/Models/Detail/ForecastView.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastView.swift
+//  AnimatedWeather
+//
+//  Created by 이동희 on 2022/10/31.
+//
+
+import SwiftUI
+
+struct ForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForecastView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForecastView()
+    }
+}
