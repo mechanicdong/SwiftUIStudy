1cedf93c9b04cf9e952ac95abfc66ffd4b784f48
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 14:13:46 +0900

    [Add] Gradient Color Extension

---
+++ AnimatedWeather/AnimatedWeather/Utils/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  AnimatedWeather
+//
+//  Created by 이동희 on 2022/10/31.
+//
+
+import SwiftUI
+
+extension Color {
+    //MARK: SwiftUI에서는 배경색을 그라데이션으로 지정할 수 있다!
+    static let background = LinearGradient(gradient: Gradient(colors: [Color("Background 1"), Color("Background 2")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+    
+    static let bottomSheetBackground = LinearGradient(gradient: Gradient(colors: [Color("Background 1").opacity(0.26), Color("Background 2").opacity(0.26)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+    
+    static let navBarBackground = LinearGradient(gradient: Gradient(colors: [Color("Background 1").opacity(0.1), Color("Background 2").opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+    
+    static let tabBarBackground = LinearGradient(gradient: Gradient(colors: [Color("Tab Bar Background 1").opacity(0.26), Color("Tab Bar Background 2").opacity(0.26)]), startPoint: .top, endPoint: .bottom)
+    
+    static let weatherWidgetBackground = LinearGradient(gradient: Gradient(colors: [Color("Weather Widget Background 1"), Color("Weather Widget Background 2")]), startPoint: .leading, endPoint: .trailing)
+    
+    static let bottomSheetBorderMiddle = LinearGradient(gradient: Gradient(stops: [.init(color: .white, location: 0), .init(color: .clear, location: 0.2)]), startPoint: .top, endPoint: .bottom)
+    
+    static let bottomSheetBorderTop = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white.opacity(0.5), .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
+    
+    static let underline = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white, .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
+    
+    static let tabBarBorder = Color("Tab Bar Border").opacity(0.5)
+    static let forecastCardBackground = Color("Forecast Card Background")
+    static let probabilityText = Color("Probability Text")
+}
