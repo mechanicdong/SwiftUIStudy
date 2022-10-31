53fb11e39946097cbbf58d0ddf1cf5e538a9a2fe
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 16:32:15 +0900

    [Add] 탭바 뷰 using path

--- AnimatedWeather/AnimatedWeather/Models/Navigation/TabBar.swift
+++ AnimatedWeather/AnimatedWeather/Models/Navigation/TabBar.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct TabBar: View {
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Arc()
+                .fill(Color.tabBarBackground)
+                .frame(height: 88)
+                .overlay { //overlay: 뷰 원본의 공간을 기준으로 그 위에 새로운 뷰를 중첩하여 쌓는 기능 ~ UIKit의 addSubview
+                    Arc()
+                        .stroke(Color.tabBarBorder, lineWidth: 0.5)
+                }
+            
+            HStack {
+                Button {
+                    action()
+                } label: {
+                    Image(systemName: "mappin.and.ellipse")
+                        .frame(width: 44, height: 44)
+                }
+                
+                Spacer()
+                
+                NavigationLink {
+                    
+                } label: {
+                    Image(systemName: "list.star")
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .font(.title2)
+            .foregroundColor(.white)
+            .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
+        }
+        .frame(maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
     }
 }
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar()
+        TabBar(action: {})
+            .preferredColorScheme(.dark)
     }
 }
