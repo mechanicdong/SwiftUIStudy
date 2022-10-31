7726b2fe2fc6bafbac6248425a6c3585fe8e60a3
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 13:48:25 +0900

    Animated Weather App Init

---
+++ AnimatedWeather/AnimatedWeather/AnimatedWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedWeatherApp.swift
+//  AnimatedWeather
+//
+//  Created by 이동희 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
