f731de1842b8a386e3f3a21700d7cfce76bc4075
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 14:32:55 +0900

    [Add] HomeView Layout

--- AnimatedWeather/AnimatedWeather/Models/Main/HomeView.swift
+++ AnimatedWeather/AnimatedWeather/Models/Main/HomeView.swift
@@ -9,12 +9,58 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            Image("House")
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding(.top, 257)
+            
+            VStack(spacing: -10) {
+                Text("서울")
+                    .font(.largeTitle)
+                
+                VStack {
+                    Text(attributedString)
+                    Text("H:24°    L:18°")
+                        .font(.title3.weight(.semibold))
+                }
+                Spacer()
+            }
+            .padding(.top, 51)
+        }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "대체로 쾌청")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "대체로 쾌청") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
