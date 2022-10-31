943343d6311491dce6c986e8dee57f54c308234b
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 17:47:49 +0900

    [ADD] Bottom View Blur and Shadow

--- AnimatedWeather/AnimatedWeather/Models/Detail/ForecastView.swift
+++ AnimatedWeather/AnimatedWeather/Models/Detail/ForecastView.swift
@@ -9,12 +9,44 @@ import SwiftUI
 
 struct ForecastView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            
+        }
+        .backgroundBlur(radius: 25, opaque: true)
+        .background(Color.bottomSheetBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 44))
+        .innerShadow(
+            shape: RoundedRectangle(cornerRadius: 44),
+            color: Color.bottomSheetBorderMiddle,
+            lineWidth: 1,
+            offsetX: 0,
+            offsetY: 1,
+            blur: 0,
+            blendMode: .overlay,
+            opacity: 1
+        )
+        .overlay {
+            Divider()
+                .blendMode(.overlay)
+                .background(Color.bottomSheetBorderTop)
+                .frame(maxHeight: .infinity, alignment: .top)
+                .clipShape(RoundedRectangle(cornerRadius: 44))
+        }
+        .overlay {
+            // Drag Indicator
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.black.opacity(0.3))
+                .frame(width: 48, height: 5)
+                .frame(height: 20)
+                .frame(maxHeight: .infinity, alignment: .top)
+        }
     }
 }
 
 struct ForecastView_Previews: PreviewProvider {
     static var previews: some View {
         ForecastView()
+            .background(Color.background)
+            .preferredColorScheme(.dark)
     }
 }
