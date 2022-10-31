6c0ce4465d0207d44315c0d097a394ac1d4ea016
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-31 17:49:02 +0900

    [ADD] UIView Wrapping for Blur(CALayer)

--- AnimatedWeather/AnimatedWeather/Models/Components/Blur.swift
+++ AnimatedWeather/AnimatedWeather/Models/Components/Blur.swift
@@ -7,9 +7,38 @@
 
 import SwiftUI
 
+class UIBackdropView: UIView {
+    override class var layerClass: AnyClass {
+        NSClassFromString("CABackdropLayer") ?? CALayer.self
+    }
+}
+
+//UIKit의 UIView를 SwiftUI에서 사용가능 한 View로 wrapping
+struct Backdrop: UIViewRepresentable {
+    /* makeUIView
+     생성되는 view의 여러가지 프로퍼티를 지정할 수 있다
+     원하는 뷰를 생성하고 리턴해주기만 하면 된다
+    */
+    func makeUIView(context: Context) -> UIBackdropView {
+        UIBackdropView()
+    }
+    /* updateUIView
+     SwiftUI View의 state가 바뀔 때마다 트리거 됨
+     이 메소드 안에서 view의 정보를 업데이트 할 수 있음
+     또한 @Binding 기능을 이용해서 SwiftUI view의 상태도 가져올 수 있다. 단, read only
+    */
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    
+    }
+}
+
 struct Blur: View {
+    var radius: CGFloat = 3
+    var opaque: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Backdrop()
+            .blur(radius: radius, opaque: opaque)
     }
 }
 
